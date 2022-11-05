3f8a8d98a60c373c6d5d2abc93f298b7a5e9acf5
Vahan Hakobyan <vahanius@gmail.com>
2022-11-05 13:22:58 +0400

    Vahan's changes

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+        <p>This is my new Branch on the Lesson 8 (Vahan)</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
