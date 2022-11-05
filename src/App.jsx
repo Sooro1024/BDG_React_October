a3a5d138f2ccbeaa701c49578c6c6f0251901cdb
Sargis <saq.mxoyan@gmail.com>
2022-11-05 13:35:25 +0400

    Hello

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>This is my new Branch on the lesson 8 (Sargis)</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
