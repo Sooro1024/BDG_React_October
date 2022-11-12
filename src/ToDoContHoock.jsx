2611b7d1bf7033e4a0b64e4e166982bf36dc67ac
Sooro1024 <soorogorgoyan@gmail.com>
2022-11-12 11:21:35 +0400

    was resolved a typeerror map is undefined

--- src/ToDoContHoock.jsx
+++ src/ToDoContHoock.jsx
@@ -5,32 +5,25 @@ export default function () {
     todos: [],
     text: "",
   });
-  console.table(state.todos)
-  //   console.log(state)
   const handleChange = (ev) => {
-    setState({ text: ev.target.value });
-    // console.log(state);
-    // console.log(setState);
+    setState((prevStat) => ({ ...prevStat, text: ev.target.value }));
   };
   const handleAdd = () => {
     setState({
       text: "",
       todos: [...state.todos, state.text],
     });
-    // console.log(state.text)
-    // console.table(state.todos)
-
 
   };
 
   return (
     <>
       <h1>To do list Hook</h1>
-      {/* <ul>
+      <ul>
           {state.todos.map((el, index) => (
             <li key={index}>{el}</li>
           ))}
-        </ul> */}
+        </ul>
       <input
         placeholder="Add todo"
         value={state.text}
