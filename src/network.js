505c1df1d2e4f048604d3c34714b4e89ee897ad1
Sooro1024 <soorogorgoyan@gmail.com>
2022-12-04 23:47:53 +0400

    was added a baseURL

--- src/network.js
+++ src/network.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const networkProvider = axios.create({
-  baseURL: "https://fa3d-78-109-64-9.ngrok.io",
+  baseURL: "https://bdg.onrender.com",
 });
 
 export { networkProvider };
